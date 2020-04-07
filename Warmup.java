429880d0ef852ac5bfe13c6f52454c0c182bf0a9
igalk1515 <igalk1515@gmail.com>
2020-04-07 15:51:34 +0300

    sad

--- Warmup.java
+++ Warmup.java
@@ -1,6 +1,24 @@
+import java.util.Stack;
+
 public class Warmup {
     public static int backtrackingSearch(int[] arr, int x, int fd, int bk, Stack myStack) {
-        // TODO: implement your code here
+    	Stack<E> st = new Stack;
+    	int f = 0;
+    	int d = 0;
+    	for (int i = 0; i < arr.length; i++) {
+    		if(x == arr[i]) {
+    			return i;
+    		}
+			st.push(arr[i]);
+			f=f+1;
+			if(f==fd) {
+				for (int b = 0; b != bk ; b++) {
+					arr[i] = st.pop();
+				}
+				f=0;
+			}
+		}
+    	return -1;
     }
 
     public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
